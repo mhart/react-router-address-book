ee10d5bfd03e0ebd29c5a5c83edf748c05baceee
Michael Hart <mhart@cloudflare.com>
2025-03-21 01:05:11 +1100

    Get working with @cloudflare/vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [cloudflare({ viteEnvironment: { name: "ssr" } }), reactRouter()],
 });
